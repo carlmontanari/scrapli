dc4cb8b2779c97dc3fff680496b81295866977a8
Carl Montanari <carl.r.montanari@gmail.com>
2020-05-24 15:56:37 -0700

    break channel out to base/normal/async, few simple tests for async drivers

--- scrapli/channel/__init__.py
+++ scrapli/channel/__init__.py
@@ -1,5 +1,6 @@
 """scrapli.channel"""
 from scrapli.channel.async_channel import AsyncChannel
-from scrapli.channel.channel import CHANNEL_ARGS, Channel
+from scrapli.channel.base_channel import CHANNEL_ARGS
+from scrapli.channel.channel import Channel
 
 __all__ = ("AsyncChannel", "Channel", "CHANNEL_ARGS")
