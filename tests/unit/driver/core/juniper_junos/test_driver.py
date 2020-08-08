9f342120d9c1de6e0acfe693ae651aac4277b8f3
Carl Montanari <carl.r.montanari@gmail.com>
2020-08-08 15:50:37 -0700

    add comment for posterity adn also to make the build run again to test it

--- tests/unit/driver/core/juniper_junos/test_driver.py
+++ tests/unit/driver/core/juniper_junos/test_driver.py
@@ -11,6 +11,8 @@ def test_junos_driver_init_telnet():
 def test_junos_open_close(sync_juniper_junos_conn):
     sync_juniper_junos_conn.open()
     assert sync_juniper_junos_conn.get_prompt() == "vrnetlab>"
+    # chalking this up to weirdness w/ the mock ssh server... it seems to not set the terminal
+    # length correct on open the first time... unclear why... future me problem
     sync_juniper_junos_conn.send_command(command="set cli screen-length 0")
     # "more" will show up if we haven't sent terminal length 0 to the mock nxos server
     assert "more" not in sync_juniper_junos_conn.send_command(command="show version").result.lower()
