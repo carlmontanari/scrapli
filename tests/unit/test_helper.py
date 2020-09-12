1a6b221424503be35764a88feb48e06993684eb9
Carl Montanari <8515611+carlmontanari@users.noreply.github.com>
2020-09-12 10:37:56 -0700

    Pre open method (#48)
    add 3.9-dev to testing
    add an on_init method that will be used for scrapli community shortly

--- tests/unit/test_helper.py
+++ tests/unit/test_helper.py
@@ -138,6 +138,9 @@ def test_textfsm_parse_failure():
     assert result == []
 
 
+@pytest.mark.skipif(
+    sys.version_info.minor > 8, reason="genie not currently available for python 3.9"
+)
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="not supporting genie on windows")
 def test_genie_parse_success():
     result = genie_parse("iosxe", "show ip arp", IOS_ARP)
@@ -147,11 +150,13 @@ def test_genie_parse_success():
     )
 
 
+@pytest.mark.skipif(
+    sys.version_info.minor > 8, reason="genie not currently available for python 3.9"
+)
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="not supporting genie on windows")
 def test_genie_parse_failure():
     result = genie_parse("iosxe", "show ip arp", "not really arp data")
     assert result == []
-    # genie loads about nine million modules... for whatever reason these two upset pyfakefs
     del sys.modules["ats.configuration"]
     del sys.modules["pyats.configuration"]
 
