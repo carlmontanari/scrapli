97802448420563137313e7a7e6925073853bfe86
Carl Montanari <8515611+carlmontanari@users.noreply.github.com>
2021-10-16 14:08:27 -0700

    fix channel log mode not getting passed to base driver, add test to make that not happen again! (#190)

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 
 import pytest
 
+from scrapli import AsyncScrapli, Scrapli
 from scrapli.channel.async_channel import AsyncChannel
 from scrapli.channel.base_channel import BaseChannel, BaseChannelArgs
 from scrapli.channel.sync_channel import Channel
@@ -568,3 +569,22 @@ def async_junos_driver():
     async_junos_driver.textfsm_platform = "juniper_junos"
     async_junos_driver.genie_platform = ""
     return async_junos_driver
+
+
+@pytest.fixture(
+    scope="class",
+    params=["cisco_iosxe", "cisco_nxos", "cisco_iosxr", "arista_eos", "juniper_junos"],
+)
+def device_type(request):
+    yield request.param
+
+
+@pytest.fixture(
+    scope="class",
+    params=["sync", "async"],
+)
+def factory(request):
+    if request.param == "sync":
+        yield Scrapli
+    else:
+        yield AsyncScrapli
