1449bc34b690b840290315350b564ea0a47ff27f
Carl Montanari <carl.r.montanari@gmail.com>
2021-10-31 09:18:55 -0700

    possible improvement for read until handling

--- tests/unit/channel/test_sync_channel.py
+++ tests/unit/channel/test_sync_channel.py
@@ -422,8 +422,8 @@ def test_send_inputs_interact(monkeypatch, sync_channel):
     _read_counter = 0
     _event_counter = 0
 
-    interact_events = [("clear logg", "[confirm]\n"), ("", "scrapli>")]
-    expected_buf = b"clear logg[confirm]\nscrapli>"
+    interact_events = [("clear logg", "[confirm]"), ("", "scrapli>")]
+    expected_buf = b"clear logg[confirm]scrapli>"
 
     def _read(cls):
         nonlocal _read_counter
