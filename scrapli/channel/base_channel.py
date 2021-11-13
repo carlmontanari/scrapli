81d32055563fbc16a562d707a3fe80a48c99da7f
Carl Montanari <carl.r.montanari@gmail.com>
2021-11-13 09:41:15 -0800

    expand in channel ssh auth password prompt pattern, changelog updates

--- scrapli/channel/base_channel.py
+++ scrapli/channel/base_channel.py
@@ -111,7 +111,7 @@ class BaseChannel:
         self.channel_log: Optional[BinaryIO] = None
 
         self._auth_telnet_login_pattern = r"^(.*username:)|(.*login:)\s?$"
-        self._auth_password_pattern = r"^password:\s?$"
+        self._auth_password_pattern = r"^(.*@.*)?password:\s?$"
         self._auth_passphrase_pattern = r"enter passphrase for key"
 
     @property
