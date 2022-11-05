d426d69f5b2d62b3e97e795baba4698b8a8e225b
Carl Montanari <carl.r.montanari@gmail.com>
2022-11-05 12:37:05 -0700

    chore: 3.11 actions/classifier updates

--- noxfile.py
+++ noxfile.py
@@ -99,7 +99,7 @@ def unit_tests(session):
     )
 
 
-@nox.session(python=["3.10"])
+@nox.session(python=["3.11"])
 def integration_tests(session):
     """
     Nox run integration tests
@@ -136,7 +136,7 @@ def integration_tests(session):
     )
 
 
-@nox.session(python=["3.10"])
+@nox.session(python=["3.11"])
 def isort(session):
     """
     Nox run isort
@@ -156,7 +156,7 @@ def isort(session):
     session.run("python", "-m", "isort", "-c", ".")
 
 
-@nox.session(python=["3.10"])
+@nox.session(python=["3.11"])
 def black(session):
     """
     Nox run black
@@ -176,7 +176,7 @@ def black(session):
     session.run("python", "-m", "black", "--check", ".")
 
 
-@nox.session(python=["3.10"])
+@nox.session(python=["3.11"])
 def pylama(session):
     """
     Nox run pylama
@@ -195,7 +195,7 @@ def pylama(session):
     session.run("python", "-m", "pylama", ".")
 
 
-@nox.session(python=["3.10"])
+@nox.session(python=["3.11"])
 def pydocstyle(session):
     """
     Nox run pydocstyle
@@ -215,7 +215,7 @@ def pydocstyle(session):
     session.run("python", "-m", "pydocstyle", ".")
 
 
-@nox.session(python=["3.10"])
+@nox.session(python=["3.11"])
 def mypy(session):
     """
     Nox run mypy
@@ -237,7 +237,7 @@ def mypy(session):
     session.run("python", "-m", "mypy", "--strict", "scrapli/")
 
 
-@nox.session(python=["3.10"])
+@nox.session(python=["3.11"])
 def darglint(session):
     """
     Nox run darglint
