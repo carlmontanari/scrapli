133386769e355f5a48591e14bcb941a62969ec40
haccht <haccht@users.noreply.github.com>
2023-01-31 12:03:09 +0900

    Add "admin_privilege_exec", "admin_configuration", "admin_configuration_exclusive" privilege for IOSXRDriver

--- scrapli/driver/core/cisco_iosxr/base_driver.py
+++ scrapli/driver/core/cisco_iosxr/base_driver.py
@@ -13,6 +13,17 @@ PRIVS = {
             escalate_prompt="",
         )
     ),
+    "admin_privilege_exec": (
+        PrivilegeLevel(
+            pattern=r"^[\w.\-@/:]{1,63}\(admin\)#\s?$",
+            name="admin_privilege_exec",
+            previous_priv="privilege_exec",
+            deescalate="exit",
+            escalate="admin",
+            escalate_auth=False,
+            escalate_prompt="",
+        )
+    ),
     "configuration": (
         PrivilegeLevel(
             pattern=r"^[\w.\-@/:]{1,63}\(config[\w.\-@/:]{0,32}\)#\s?$",
@@ -31,6 +42,28 @@ PRIVS = {
             previous_priv="privilege_exec",
             deescalate="end",
             escalate="configure exclusive",
+            escalate_auth=False,
+            escalate_prompt="",
+        )
+    ),
+    "admin_configuration": (
+        PrivilegeLevel(
+            pattern=r"^[\w.\-@/:]{1,63}\(admin-config[\w.\-@/:]{0,32}\)#\s?$",
+            name="admin_configuration",
+            previous_priv="privilege_exec",
+            deescalate="end",
+            escalate="admin configure terminal",
+            escalate_auth=False,
+            escalate_prompt="",
+        )
+    ),
+    "admin_configuration_exclusive": (
+        PrivilegeLevel(
+            pattern=r"^[\w.\-@/:]{1,63}\(admin-config[\w.\-@/:]{0,32}\)#\s?$",
+            name="admin_configuration_exclusive",
+            previous_priv="privilege_exec",
+            deescalate="end",
+            escalate="admin configure exclusive",
             escalate_auth=False,
             escalate_prompt="",
         )
