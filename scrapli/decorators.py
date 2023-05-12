a047b03da0c08ba6302c25943252d7b52ad08053
v.garkaviy <v.garkaviy@yadro.com>
2023-05-12 15:35:05 +0300

    restore timeout_ops whatever happened

--- scrapli/decorators.py
+++ scrapli/decorators.py
@@ -279,8 +279,10 @@ def timeout_modifier(wrapped_func: Callable[..., Any]) -> Callable[..., Any]:
                 )
                 base_timeout_ops = driver_instance.timeout_ops
                 driver_instance.timeout_ops = kwargs["timeout_ops"]
-                result = await wrapped_func(*args, **kwargs)
-                driver_instance.timeout_ops = base_timeout_ops
+                try:
+                    result = await wrapped_func(*args, **kwargs)
+                finally:
+                    driver_instance.timeout_ops = base_timeout_ops
             return result
 
     else:
