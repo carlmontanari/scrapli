f65104f1e2dbd875c91ae8522ab4dcb854391033
Carl Montanari <carl.r.montanari@gmail.com>
2024-11-21 10:42:44 -0800

    chore: bump ver for post release

--- scrapli/__init__.py
+++ scrapli/__init__.py
@@ -3,7 +3,7 @@
 from scrapli.driver.base import AsyncDriver, Driver
 from scrapli.factory import AsyncScrapli, Scrapli
 
-__version__ = "2024.07.30"
+__version__ = "2024.07.30.post1"
 
 __all__ = (
     "AsyncDriver",
