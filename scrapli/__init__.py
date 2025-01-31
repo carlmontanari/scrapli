06580feeb029f144d0ca16696bf7358c460f2349
Carl Montanari <carl.r.montanari@gmail.com>
2025-01-30 16:35:55 -0800

    chore: bump versions for ci things, prep for release

--- scrapli/__init__.py
+++ scrapli/__init__.py
@@ -3,7 +3,7 @@
 from scrapli.driver.base import AsyncDriver, Driver
 from scrapli.factory import AsyncScrapli, Scrapli
 
-__version__ = "2024.07.30.post1"
+__version__ = "2025.01.30"
 
 __all__ = (
     "AsyncDriver",
