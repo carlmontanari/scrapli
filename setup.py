bfc227e4fbd0cce4884b7064adf91e797d0176fa
Carl Montanari <carl.r.montanari@gmail.com>
2025-05-12 17:32:48 -0700

    fix: dont relative import libscrapli ver in setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@ from setuptools.command.bdist_wheel import bdist_wheel
 from setuptools.command.editable_wheel import editable_wheel
 from setuptools.command.sdist import sdist
 
-from .scrapli.ffi import LIBSCRAPLI_VERSION
-
+LIBSCRAPLI_VERSION = "0.0.1-alpha.4"
 LIBSCRAPLI_REPO = os.environ.get("LIBSCRAPLI_REPO", "https://github.com/scrapli/libscrapli")
 LIBSCRAPLI_TAG = os.environ.get("LIBSCRAPLI_TAG", f"v{LIBSCRAPLI_VERSION}")
 LIBSCRAPLI_BUILD_PATH_ENV = "LIBSCRAPLI_BUILD_PATH"
