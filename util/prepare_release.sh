173f3899408ea2a2fe60be355a3a199ee591bcd2
Carl Montanari <carl.r.montanari@gmail.com>
2025-05-17 12:03:57 -0700

    chore: add bump scrapli (py) version to the prep release script

--- util/prepare_release.sh
+++ util/prepare_release.sh
@@ -10,6 +10,8 @@ LATEST_TAGS=(
         | head -n 5)
 )
 
+CURRENT_SCRAPLI_VERSION=$(grep -Eo '__version__\s=\s"[^"]+"' scrapli/__init__.py | cut -d '"' -f2)
+
 prompt_for_tag() {
   echo "recent tags:"
   select version in "${LATEST_TAGS[@]}"; do
@@ -32,8 +34,15 @@ else
     prompt_for_tag
 fi
 
+echo "current scrapli version is: $CURRENT_VERSION"
+read -p "enter new version (or enter to keep current): " NEW_VERSION
+
 sed -i.bak -E "s|(LIBSCRAPLI_VERSION = )(.*)|\1\"${TARGET_TAG#v}\"|g" scrapli/ffi.py
 sed -i.bak -E "s|(LIBSCRAPLI_VERSION = )(.*)|\1\"${TARGET_TAG#v}\"|g" setup.py
+
+if [[ -n "$NEW_VERSION" ]]; then
+  sed -i.bak -E "s|(__version__\s*=\s*\")[^\"]+(\".*)|\1${NEW_VERSION}\2|g" scrapli/__init__.py
+fi
 
 git diff -- scrapli/ffi.py setup.py
 
@@ -42,8 +51,10 @@ read -p "looks good? (y/n): " confirm
 if [[ "$confirm" == [yY] ]]; then
   rm scrapli/ffi.py.bak
   rm setup.py.bak
+  rm scrapli/__init__.py.bak
 else
   echo "restoring...."
   mv scrapli/ffi.py.bak scrapli/ffi.py
   mv setup.py.bak setup.py
+  mv scrapli/__init__.py.bak scrapli/__init.py
 fi
