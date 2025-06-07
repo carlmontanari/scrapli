c02c9b1ebb126fb2ce46f60097cb141f27b8003e
Carl Montanari <carl.r.montanari@gmail.com>
2025-06-07 10:41:06 -0700

    chore: repin

--- scrapli/__init__.py
+++ scrapli/__init__.py
@@ -12,8 +12,8 @@ from scrapli.transport import Ssh2Options as TransportSsh2Options
 from scrapli.transport import TelnetOptions as TransportTelnetOptions
 from scrapli.transport import TestOptions as TransportTestOptions
 
-__version__ = "2.0.0-alpha.5"
-__calendar_version__ = "2025.6.6"
+__version__ = "2.0.0-alpha.6"
+__calendar_version__ = "2025.6.7"
 __definitions_version__ = "471f12e"
 
 __all__ = (
