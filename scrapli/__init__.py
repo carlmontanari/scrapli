3d80c1974a19cf12ff32f1f1f534e6ee67ae6acb
Carl Montanari <carl.r.montanari@gmail.com>
2025-06-08 09:51:44 -0700

    chore: ver, pre release again

--- scrapli/__init__.py
+++ scrapli/__init__.py
@@ -12,7 +12,7 @@ from scrapli.transport import Ssh2Options as TransportSsh2Options
 from scrapli.transport import TelnetOptions as TransportTelnetOptions
 from scrapli.transport import TestOptions as TransportTestOptions
 
-__version__ = "2.0.0-alpha.6"
+__version__ = "2.0.0-alpha.4"
 __calendar_version__ = "2025.6.7"
 __definitions_version__ = "471f12e"
 
