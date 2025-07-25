c3f159582995bb4c9c1525153c12d1afdfd2760c
Carl Montanari <carl.r.montanari@gmail.com>
2025-07-25 06:15:59 -0700

    chore: repin definitions

--- scrapli/__init__.py
+++ scrapli/__init__.py
@@ -13,7 +13,7 @@ from scrapli.transport import TestOptions as TransportTestOptions
 
 __version__ = "2.0.0-alpha.5"
 __calendar_version__ = "2025.7.12"
-__definitions_version__ = "c38ec6d"
+__definitions_version__ = "1a3d771"
 
 __all__ = (
     "AuthOptions",
